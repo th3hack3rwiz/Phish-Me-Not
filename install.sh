e39e8cac48e103e5e92dc8ed3b70121bd36f2e32
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2021-02-26 01:32:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 mv index.html /var/www/html/
+read -p "Enter the emailID of the sender with which you wish to send all your phishing emails from: " email
+read -p "Enter the password of that email account: " pass
+echo "export EMAILID='$email'" >> ~/.bashrc
+echo "export PASS='$pass'" >> ~/.bashrc
+source ~/.bashrc
 go get -u github.com/tomnomnom/anew
 
