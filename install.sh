6429ac3883befd7042254cf77f38f369870ea1d6
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2021-02-26 06:00:08 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 mv index.html /var/www/html/
+sed -i "s#XXXX#$(pwd)/configure-phishing-simulator.sh#" phish-me-not.py
+sed -i "s#YYYY#$(pwd)/retrieve-phished-employee-intel.sh#" phish-me-not.py
 read -p "Enter the sender's email-ID. All phishing mails will be sent with this email address: " email
 read -p "Enter the password of that email account: " pass
 echo "export EMAILID='$email'" >> ~/.bashrc
