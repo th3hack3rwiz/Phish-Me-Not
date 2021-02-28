8b40fa246415dee1a2716d6e5ea89858dd9733a5
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2021-02-28 07:36:54 +0530

    Added ip-location URL
    Added names of the phished employees and also the URL to get to their exact location in results.txt.

--- retrieve-phished-employee-intel.sh
+++ retrieve-phished-employee-intel.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 rm results.txt>/dev/null 2>&1
 rm ips>/dev/null 2>&1
-# rm adminURL >/dev/null 2>&1
-# rm victimURL > /dev/null 2>&1
 RED='\e[38;5;196m'
 GREEN='\e[38;5;46m' 
 GOLD='\e[38;5;226m'
 GREY='\033[0;37m'
 CYAN='\e[38;5;14m'
-# echo -e "${RED}${BOLD}$(figlet -f slant  'PHISH-ME-NOT!')"
+
 count=0 
-# pingb=$(curl pingb.in 2>&1 | cut -d "\"" -f 2 | xargs | sed "s/.*\//\//g" | xargs -I{} echo {})
 url=$(cat adminURL)
-# victimURL="http://pingb.in/p$pingb"
-# echo ${victimURL} > victimURL
-# echo -e "${GOLD}[+] Send this to victim: ${RED}${victimURL}${GOLD}"
+
 while true; do
 	 printf "\n"
 	 read -p '[?] Press / 1 to REFRESH the LIST / 2 to EXIT: ' user_input 
@@ -23,10 +18,15 @@ while true; do
 		  wget $url --wait=3 -U 'Mozilla/5.0 (X11; Linux i686 (x86_64)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36' --no-http-keep-alive --no-check-certificate --tries=1 -O test1.html > /dev/null 2>&1 
 		  cat test1.html | grep -E '([0-9]{1,3}[\.]){3}[0-9]{1,3}' | sed 's/<td>//g' | sed 's/<\/td>//g' | sed 's/^[[:space:]]*//g' | sed 's/:/ /g' | sort -u | anew ips_ports > new_ips_ports ; echo -e "\n${GREEN}[+] Log $(echo $count):" 
 		  if [ ! -s results.txt ] ; then
-			echo "IP Port Country State City Latitude Longitude Zip_Code Time_Zone ISP Domain Is_Proxy? Proxy_Type" > results.txt
-	      fi
+			echo "Victim_Employee IP Port Country State City Latitude Longitude Zip_Code Time_Zone ISP Domain Is_Proxy? Proxy_Type Geo_URL" > results.txt
+	 	  fi
 		  while read i; do # to fetch only new IPs info
 		  	ip=$(echo $i | awk '{print $1}')
+			cat employee-table.txt| grep $ip > /dev/null	#can replace employee table with path
+		  	if [ $? -eq 0 ]; then
+				name=$(cat employee-table.txt| grep $ip | awk '{print $1}')
+		 	else name="_" 
+			fi
 		  	port=$(echo $i | awk '{print $2}')
 			echo -e "${GREEN}[+] Updating proxy info..."
 		  	wget "https://api.ip2proxy.com/?key=demo&ip=${ip}&package=PX10&format=json&&addon=geotargeting" --wait=3 -U 'Mozilla/5.0 (X11; Linux i686 (x86_64)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36' --no-http-keep-alive --no-check-certificate --tries=1 -O test3.html > /dev/null 2>&1
@@ -38,8 +38,11 @@ while true; do
 		  	wget "https://api.ip2location.com/v2/?ip=$ip&key=demo&package=WS24&addon=continent,country,region,city,geotargeting,country_groupings,time_zone_info" --wait=3 -U 'Mozilla/5.0 (X11; Linux i686 (x86_64)) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.75 Safari/537.36' --no-http-keep-alive --no-check-certificate --tries=1 -O test4.html > /dev/null 2>&1
 		  	cat test4.html | anew apiGeo-Location > geo
 
-		  	intel=$(cat geo | jq | grep -E '"country_name":|"city_name":|"zip_code":|"time_zone":|"isp":|"domain":|"region_name"|"latitude"|"longitude"' | sed 's/,//g'  | sed 's/"//g' | awk -F ": " '{print $2}' | sed 's/[[:space:]]/_/g' | xargs)	  	
-		    echo "$ip $port $intel $isProxy $proxyType" | anew -q results.txt 
+		  	intel=$(cat geo | jq | grep -E '"country_name":|"city_name":|"zip_code":|"time_zone":|"isp":|"domain":|"region_name"|"latitude"|"longitude"' | sed 's/,//g'  | sed 's/"//g' | awk -F ": " '{print $2}' | sed 's/[[:space:]]/_/g' | xargs)
+		  lat=$(echo $intel | awk '{print $4}')
+		  long=$(echo $intel | awk '{print $5}')
+		  geoURL="https://www.gps-coordinates.net/latitude-longitude/$lat/$long/10/roadmap"
+		    echo "$name $ip $port $intel $isProxy $proxyType $geoURL" | anew -q results.txt 
 		  done < new_ips_ports
 		  printf "\n"
 		  cat results.txt | column -t
