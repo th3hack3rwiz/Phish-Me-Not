dbd4b869981d20a18c8a30f91ce2c381247bb869
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2021-03-02 10:58:30 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo mv index.html /var/www/html/
 chmod +x configure-phishing-simulator.sh
 chmod +x retrieve-phished-employee-intel.sh
-sudo apt get install jq
+sudo apt-get install jq
 sed -i "s#XXXX#$(pwd)/configure-phishing-simulator.sh#" phish-me-not.py
 sed -i "s#YYYY#$(pwd)/retrieve-phished-employee-intel.sh#" phish-me-not.py
 read -p "Enter the sender's email-ID. All phishing mails will be sent with this email address: " email
@@ -10,11 +10,11 @@ read -p "Enter the password of that email account: " pass
 if [[ -f ~/.zshrc ]]; then 
         echo "export EMAILID='$email'" >> ~/.zshrc
         echo "export PASS='$pass'" >> ~/.zshrc
-        source ~/.zshrc
+        source ~/.zshrc >/dev/null 2>&1
 else
         echo "export EMAILID='$email'" >> ~/.bashrc
         echo "export PASS='$pass'" >> ~/.bashrc
-        source ~/.bashrc
+        source ~/.bashrc >/dev/null 2>&1
 fi
 sudo pip3 install xlsxwriter --upgrade 
 sudo apt-get install libreoffice
