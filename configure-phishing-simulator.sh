cacf103de725e50381961918014cbedbd641685b
Dhruv Kandpal <56139983+th3hack3rwiz@users.noreply.github.com>
2021-03-14 14:11:37 +0530

    Update configure-phishing-simulator.sh
    Handled cases where:-
    1) NGROK failed to start. 
    2) PingB failed to generate a unique URL.

--- configure-phishing-simulator.sh
+++ configure-phishing-simulator.sh
@@ -11,14 +11,29 @@ echo -e "${GOLD}${BOLD}$(figlet -f slant  PHISH-ME-NOT!)"
 echo -e "${RED}${BOLD}\t\t\t\t-Employee Phishing Simulator."
 echo -e "${GREEN}[+] Starting Apache server"
 service apache2 start
-echo -e "${GREEN}[+] Hosting local server on the internet through NGROK"
+echo -e "${GREEN}[+] Hosting the local server on the internet through NGROK"
 ngrok tcp 80 --log=stdout >log & sleep 7 >/dev/null 2>&1  
 cat log| grep url= | awk -F "=" '{print $8}' >ngrokURL
 rm -f log
 sed -i 's/tcp:/http:/g' ngrokURL
+if [ ! -s ngrokURL ]; then
+        echo -e "${RED}[-] NGROK operation failed! :( Try again."
+        disown
+	sudo killall -9 ngrok &>/dev/null
+        rm -f ngrokURL
+        exit 1
+fi
+echo -e "${GREEN}[+] Creating a unique pingB URL and embedding it inside an iframe in index.html file."
 pingb=$(curl pingb.in 2>&1 | cut -d "\"" -f 2 | xargs | sed "s/.*\//\//g" | xargs -I{} echo {})
+
+if [ -z $pingb ]; then
+echo -e "${RED}[-] Failed to create a unique phishing URL! :( Try again."
+sudo killall -9 ngrok &>/dev/null
+exit 1
+fi
+
 url="http://pingb.in$pingb"
 victimURL="http://pingb.in/p$pingb"
-sudo sed -i "s#REPLACE_ME#${victimURL}#g" /var/www/html/index.html
 echo ${url} > adminURL
 echo $victimURL > victimURL
+sudo sed -i "s#REPLACE_ME#${victimURL}#g" /var/www/html/index.html
